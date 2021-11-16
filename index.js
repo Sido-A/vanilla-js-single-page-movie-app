c79567d1a5bf8132d55ed1958e2323faa443971e
Sido-A <sidonie.abe.uk@gmail.com>
2021-11-16 19:42:38 +0000

    added function to render movie per genre

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const BASE_URL = `https://api.themoviedb.org/3`;
 const API_KEY = `?api_key=c33575ea5f8a18616196a44b1637bda6`;
 
-//trending
 const TRENDING = `${BASE_URL}/trending/all/week${API_KEY}`;
-//upcoming
-const UPCOMING = `${BASE_URL}/movie/upcoming${API_KEY}`;
-//genre
 const GENRES = `${BASE_URL}/genre/movie/list${API_KEY}`;
-//top rated
+const LATEST = `${BASE_URL}/movie/latest${API_KEY}`;
+const NOW_PLAYING = `${BASE_URL}/movie/now_playing${API_KEY}`;
+const POPULAR = `${BASE_URL}/movie/popular${API_KEY}`;
 const TOP_RATED = `${BASE_URL}/movie/top_rated${API_KEY}`;
+const UPCOMING = `${BASE_URL}/movie/upcoming${API_KEY}`;
+
+let MOVIES = [];
 
 const render_movie_card = (movies, element_id) => {
   const element = document.getElementById(`${element_id}`);
@@ -23,28 +24,75 @@ const render_movie_card = (movies, element_id) => {
   });
 };
 
+const render_movie_card_per_genre = (movies, element_id) => {
+  const element = document.getElementById(`${element_id}`);
+  element.classList.add("show");
+  element.innerHTML = `<div class="close-wrapper">
+  <button class="close" onclick="close_movie_details_on_click()">
+    x
+  </button>
+  </div>
+  <div class="movie-per-genre-container">
+
+  </div>
+`;
+
+  const body_element = document.querySelector(".movie-per-genre-container");
+  movies.forEach((movie) => {
+    const movie_img = `https://image.tmdb.org/t/p/w185_and_h278_bestv2${movie.poster_path}`;
+    body_element.innerHTML += `
+        
+            <div
+                onclick='show_movie_details_on_click(${movie.id})'
+                class="movie-per-genre-card"
+                >
+                <div class="movie-per-genre-card-header"></div>
+                <div class="movie-per-genre-card-body">
+                    <img src='${movie_img}' />
+                </div>
+            </div>
+        `;
+  });
+};
+
+const infinite_scroll = (element) => {
+  const content_element = document.getElementById(`${element}`);
+  content_element.addEventListener("scroll", (e) => {
+    let max_possible_scroll = content_element.scrollLeftMax;
+    if (e.target.scrollLeft >= max_possible_scroll) {
+      content_element.childNodes.forEach((child) => {
+        const cloned_content = child.cloneNode(true);
+        content_element.appendChild(cloned_content);
+      });
+    }
+  });
+};
+
 fetch(UPCOMING)
   .then((res) => res.json())
   .then((movies) => {
-    console.log(movies.results);
+    // console.log("UPCOMING", movies.results);
+    infinite_scroll("upcoming");
     render_movie_card(movies.results, "upcoming");
   });
 
 fetch(TRENDING)
   .then((res) => res.json())
   .then((movies) => {
-    // console.log(movies.results);
+    // console.log("TRENDING", movies.results);
+    MOVIES = [...MOVIES, ...movies.results];
+    infinite_scroll("trending");
     render_movie_card(movies.results, "trending");
   });
 
 fetch(GENRES)
   .then((res) => res.json())
   .then((genres_arr) => {
-    // console.log(genres_arr);
+    // console.log("GENRES", genres_arr);
     const element = document.getElementById(`genres`);
     genres_arr.genres.forEach((genre) => {
       element.innerHTML += `
-          <div class="card genre-card" id="genre-${genre.id}">
+          <div onclick='show_movie_per_genre(${genre.id})' class="card genre-card" id="genre-${genre.id}">
             <div class="card-header"></div>
             <div class="card-body">${genre.name}</div>
         </div>`;
@@ -54,8 +102,17 @@ fetch(GENRES)
 fetch(TOP_RATED)
   .then((res) => res.json())
   .then((movies) => {
-    // console.log(movies.results);
+    // console.log("TOP_RATED",movies.results);
+    MOVIES = [...MOVIES, ...movies.results];
+    infinite_scroll("top-rated");
     render_movie_card(movies.results, "top-rated");
+  });
+
+fetch(POPULAR)
+  .then((res) => res.json())
+  .then((movies) => {
+    // console.log(""POPULAR,movies.results);
+    MOVIES = [...MOVIES, ...movies.results];
   });
 
 const show_movie_details_on_click = (id) => {
@@ -68,8 +125,8 @@ const show_movie_details_on_click = (id) => {
       movie_details_element.classList.add("show");
 
       movie_details_element.innerHTML = `
-      <div  class="movie-details-close">
-          <button onclick='close_movie_details_on_click()' class="close">x</button>
+      <div  class="close-wrapper">
+          <button class="close" onclick='close_movie_details_on_click()'>x</button>
       </div>
       <div class="movie-details-header">
           <h3>${movie.original_title}</h3>
@@ -90,6 +147,17 @@ const show_movie_details_on_click = (id) => {
 
 const close_movie_details_on_click = () => {
   document.querySelector("body").style.overflow = "initial";
-  const movie_details_element = document.getElementById("movie-details");
-  movie_details_element.classList.remove("show");
+  const close_button_element = document.querySelector(".close-wrapper");
+  console.log(close_button_element);
+  close_button_element.parentElement.classList.remove("show");
+};
+
+const show_movie_per_genre = (genre_id) => {
+  console.log(genre_id);
+  const movies_new_arr = MOVIES.filter((movie) =>
+    movie.genre_ids.includes(genre_id)
+  );
+  console.log(MOVIES);
+  console.log(movies_new_arr);
+  render_movie_card_per_genre(movies_new_arr, "movie-per-genre");
 };
